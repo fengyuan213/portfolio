8e6949dcb9f3660e1c8b56de5eb86628a7db4730
fengyuan213 <26819159+fengyuan213@users.noreply.github.com>
2025-03-10 02:27:15 +1030

    Configure Vercel deployment adapter for SvelteKit project

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
